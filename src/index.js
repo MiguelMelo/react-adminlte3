51314ba7de7623ec50645b48a1b92ef0dc04ca82
Carlos Miguel <miguel21.melo21@gmail.com>
2019-10-26 21:53:49 -0300

    webpack.config.js moved and separated into config, server folder created, static folder created

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello React,Webpack 4 & Babel 7!</div>;
+  return <div>React Boilerplate!</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.getElementById("app"));
