57f9403b371f57fa905f21fb8b4199c1e5034d86
Carlos Miguel <miguel21.melo21@gmail.com>
2019-10-29 20:01:13 -0300

    config: prettier, eslint, editorconfig and husky

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return <div>React Boilerplate!</div>;
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
